33d8e78abbe8ae9de9e3ea10fc4af1d3ecac5687
jmateusq <josequeiroz@estudante.ufscar.br>
2025-11-29 19:20:25 -0300

    correção de erros da gabi

--- Configracao.cpp
+++ Configracao.cpp
@@ -1,24 +1,26 @@
 #include "Configuracao.h"
 
-Contexto:Contexto() : dias(5), turnos(6), maxIteracao(5000), aulasTurno(2) {}
+Contexto::Contexto() : dias(5), turnos(6), maxIteracoes(5000), aulasPorTurno(2) {}
 
-Contexto::Contexto(int dias, int turnos, int maxInteracao, int aulasTurno)
-    : dias(), turnos(), maxInteracao(), aulasTurno()    {}
+Contexto::Contexto(int Dias, int Turnos, int MaxInteracao, int AulasTurno)
+    : dias(Dias), turnos(Turnos), maxIteracoes(MaxInteracao), aulasPorTurno(AulasTurno)    {}
 
-int getDias () const {
-        return this->dias;
-    }
-    int getTurnos () const {
-        return this->turnos;
-    }
+int Contexto::getDias() const {
+    return this->dias;
+}
+    
+int Contexto::getTurnos() const {
+    return this->turnos;
+}
 
-    int getMaxIteracao () const {
-        return this->maxIteracoes;
-    }
+int Contexto::getMaxIteracao() const {
+    return this->maxIteracoes;
+}
 
-    int getAulaTurno() const{
-        return this->aulasPorTurno;
-    }
-    int getTotalSlots() const {
-        return dias * turnos * aulasPorTurno; 
-    }
+int Contexto::getAulaTurno() const{
+    return this->aulasPorTurno;
+}
+
+int Contexto::getTotalSlots() const {
+    return dias * turnos * aulasPorTurno; 
+}
--- Grade.h
+++ Grade.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <iostream>
 #include "Materia.h"
+#include "Configuracao.h"
 
 class Grade {
 private:
