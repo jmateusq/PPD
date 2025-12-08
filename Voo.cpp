45f6d38d887fbbdbb284cfc170d86d61d298e302
jmateusq <josequeiroz@estudante.ufscar.br>
2025-12-08 18:42:39 -0300

    implementação final do algoritmo sequencial

--- Voo.cpp
+++ Voo.cpp
@@ -22,14 +22,36 @@ bool Voo::operator!=(const Voo& other) const {
 
 std::string Voo::getNomeAeroporto(int id) {
     switch(id) {
-        case 0: return "GRU";
-        case 1: return "MIA";
-        case 2: return "JFK";
-        case 3: return "LHR";
-        case 4: return "BSB";
-        case 5: return "SCL";
-        case 6: return "CDG";
-        case 7: return "DXB";
+        // Sudeste
+        case 0: return "GRU (Guarulhos)";
+        case 1: return "CGH (Congonhas)";
+        case 2: return "VCP (Viracopos)";
+        case 3: return "GIG (Galeao)";
+        case 4: return "SDU (Santos Dumont)";
+        case 5: return "CNF (Confins)";
+        case 6: return "VIX (Vitoria)";
+        // Sul
+        case 7: return "CWB (Curitiba)";
+        case 8: return "POA (Porto Alegre)";
+        case 9: return "FLN (Florianopolis)";
+        case 10: return "NVT (Navegantes)";
+        // Centro-Oeste
+        case 11: return "BSB (Brasilia)";
+        case 12: return "GYN (Goiania)";
+        case 13: return "CGB (Cuiaba)";
+        case 14: return "CGR (Campo Grande)";
+        // Nordeste
+        case 15: return "SSA (Salvador)";
+        case 16: return "REC (Recife)";
+        case 17: return "FOR (Fortaleza)";
+        case 18: return "NAT (Natal)";
+        case 19: return "MCZ (Maceio)";
+        case 20: return "SLZ (Sao Luis)";
+        // Norte
+        case 21: return "MAO (Manaus)";
+        case 22: return "BEL (Belem)";
+        case 23: return "PMW (Palmas)";
+        case 24: return "PVH (Porto Velho)";
         default: return "Aero_" + std::to_string(id);
     }
 }
